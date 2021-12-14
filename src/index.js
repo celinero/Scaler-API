75a0cb25e92ce590791607b793601065240f9c29
celine.roure <celine.roure.pro@gmail.com>
2021-12-14 19:16:02 +1100

    install firebase and firebase admin + creates user functions and user routes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const HOST = '0.0.0.0';
 
+
+const firebaseAdmin = require('firebase-admin');
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS))
+})
+
+
+
 // receive JSON data from POST/PUT/PATCH/DELETE requests
 app.use(express.json());
 
@@ -21,6 +29,9 @@ app.get('/', (request, response) => {
 
 const importedTicketRouting = require('./Tickets/ticketsRoutes');
 app.use('/tickets', importedTicketRouting);
+
+const importedUserRouting = require('./Users/userRoutes');
+app.use('/users', importedUserRouting);
 
 app.listen(PORT, HOST, () => {
   console.log("Server is running!");
